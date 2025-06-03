218ddb8ee459d337198d5725eba2d193cd304d10
Pasindu_SD <74785686+Pasindu-sd@users.noreply.github.com>
2025-06-03 21:30:45 +0530

    Delete week 05/SQL Injection directory

--- week 05/SQL Injection/admin.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if (!isset($_SESSION['username'])) {
-   header('Location: index.php?error=PleaseLogin');
-} else {
-   $username = $_SESSION['username'];
-}
-
-echo "<h1>Hello $username</h1>";
